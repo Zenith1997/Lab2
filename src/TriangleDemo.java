4af2cbe5ce97625d9a03dc52a0b81ce873296b0d
Dabarera <75031785+Zenith1997@users.noreply.github.com>
2021-09-27 21:24:01 +0530

    added commets to mak code more readable

--- src/TriangleDemo.java
+++ src/TriangleDemo.java
@@ -13,7 +13,8 @@ class Triangle {
 
     double FindHypotenuse()
     {
-        hypotenuse = Math.sqrt(Math.pow(base,2)+Math.pow(height,2));
+        hypotenuse = Math.sqrt(Math.pow(base,2)+Math.pow(height,2)); // no need to make a local variable for hypo
+        //tenuse because we have already declared an instance variable inside class Triangle.
         return hypotenuse;
 
     }
@@ -27,13 +28,14 @@ class Triangle {
 }
 class TriangleDemo{
     public static void main(String[] args) {
-        Triangle obj = new Triangle();
-        obj.height=4.0;
+        Triangle obj = new Triangle(); //Making an instance of traingle class inside the TriangleDemo class
+        obj.height=4.0; //adding values for states int he triangle class
         obj.base = 3.0;
 
+        //Calling methods in the triangle class
         System.out.println("Area of the triangle is :"+obj.FindArea());
-        System.out.println("Hypotenuse of the traingle is :"+obj.FindHypotenuse());
-        System.out.println("Perimeter of the traingle is :"+ obj.FindPerimeter());
+        System.out.println("Hypotenuse of the triangle is :"+obj.FindHypotenuse());
+        System.out.println("Perimeter of the triangle is :"+ obj.FindPerimeter());
     }
 
 }
